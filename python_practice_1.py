111ea1c0d742e83d15ca10e35d745b42d1708ea7
jstmpelowycz <dmytro.pelovych@gmail.com>
2024-02-23 14:26:30 +0200

    Misc

--- python_practice_1.py
+++ python_practice_1.py
@@ -1,9 +1,8 @@
 def greet_teacher():
-  my_name = 'Dmytro Pelovych'
-  my_group = 'SE-5'
+    my_name = 'Dmytro Pelovych'
+    my_group = 'SE-5'
 
-  print(f'{my_name}, {my_group}')
+    print(f'{my_name}, {my_group}')
 
-
-if __name__ == '__main__':
-  greet_teacher()
+# if __name__ == '__main__':
+#   greet_teacher()
